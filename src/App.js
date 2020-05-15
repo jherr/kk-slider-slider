fdde72d05750ed42090702e1ff7f940436edfc3e
Jack Herrington <jherr@pobox.com>
2020-05-15 08:14:54 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import { Swipeable } from "react-swipeable";
+import "./App.css";
 
-function App() {
+function App({ slides }) {
+  const [position, positionSet] = useState(0);
+  const handleSwipe = ({ dir }) => {
+    if (dir === "Right") {
+      if (position < slides.length - 1) {
+        positionSet(position + 1);
+      }
+    }
+    if (dir === "Left") {
+      if (position > 0) {
+        positionSet(position - 1);
+      }
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Swipeable onSwiped={handleSwipe} className="App">
+      <div className="row">
+        {slides.map((url, index) => (
+          <motion.div
+            className="container"
+            key={index}
+            initial={{ scale: 0, rotation: -180 }}
+            animate={{
+              rotate: 0,
+              left: `${(index - position) * 60 - 30}vw`,
+              scale: index === position ? 1 : 0.8,
+            }}
+            transition={{
+              type: "spring",
+              stiffness: 260,
+              damping: 20,
+            }}
+          >
+            <img src={url}></img>
+          </motion.div>
+        ))}
+      </div>
+    </Swipeable>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      slides={[
+        "/albums/cover1.png",
+        "/albums/cover2.png",
+        "/albums/cover3.png",
+        "/albums/cover4.png",
+        "/albums/cover5.png",
+        "/albums/cover6.png",
+        "/albums/cover7.png",
+        "/albums/cover8.png",
+      ]}
+    />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
